1b885376c20f3ab432af4ac66ea00f2d7311605c
Richie S <richie.shii@gmail.com>
2021-01-12 17:09:57 -0500

    installed bootstrapPPPPPPPPPPPPPPPPP

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
       <Head>
         <title>Richie Shi</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
       </Head>
       
       <Intro/>
